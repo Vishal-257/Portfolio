755cad6744cefe088ed311d27ffc704f0b03748e
Vishal-257 <vishalkumar191383527@gmail.com>
2025-10-17 17:38:42 +0530

    Project data update

--- src/components/projectsData.tsx
+++ src/components/projectsData.tsx
@@ -4,7 +4,7 @@ export const Projects = [
     title: "Quiz App",
     src: "/componentsAssests/Quiz_App.png",
     ctaText: "Go to Github",
-    ctaLink: "https://ui.aceternity.com/templates",
+    ctaLink: "",
     content: () => {
       return (
         <p>
@@ -18,7 +18,7 @@ export const Projects = [
     title: "Weather App",
     src: "/componentsAssests/Weather_App.png",
     ctaText: "Play",
-    ctaLink: "https://ui.aceternity.com/templates",
+    ctaLink: "https://github.com/Vishal-257/Weather_App",
     content: () => {
       return (
         <p>
@@ -41,7 +41,7 @@ export const Projects = [
     title: "Blog App",
     src: "/componentsAssests/Blog_App.png",
     ctaText: "Play",
-    ctaLink: "https://ui.aceternity.com/templates",
+    ctaLink: "https://github.com/Vishal-257/Blog_App_Using_OwnAPI",
     content: () => {
       return (
         <p>
@@ -54,49 +54,6 @@ export const Projects = [
           career spanning over four decades, Metallica has released numerous hit
           albums and singles that have garnered them a massive fan following
           both in the United States and abroad.
-        </p>
-      );
-    },
-  },
-  {
-    description: "Led Zeppelin",
-    title: "Stairway To Heaven",
-    src: "https://assets.aceternity.com/demos/led-zeppelin.jpeg",
-    ctaText: "Play",
-    ctaLink: "https://ui.aceternity.com/templates",
-    content: () => {
-      return (
-        <p>
-          Led Zeppelin, a legendary British rock band, is renowned for their
-          innovative sound and profound impact on the music industry. Formed in
-          London in 1968, they have become a cultural icon in the rock music
-          world. <br /> <br /> Their songs often reflect a blend of blues, hard
-          rock, and folk music, capturing the essence of the 1970s rock era.
-          With a career spanning over a decade, Led Zeppelin has released
-          numerous hit albums and singles that have garnered them a massive fan
-          following both in the United Kingdom and abroad.
-        </p>
-      );
-    },
-  },
-  {
-    description: "Mustafa Zahid",
-    title: "Toh Phir Aao",
-    src: "https://assets.aceternity.com/demos/toh-phir-aao.jpeg",
-    ctaText: "Play",
-    ctaLink: "https://ui.aceternity.com/templates",
-    content: () => {
-      return (
-        <p>
-          &quot;Aawarapan&quot;, a Bollywood movie starring Emraan Hashmi, is
-          renowned for its intense storyline and powerful performances. Directed
-          by Mohit Suri, the film has become a significant work in the Indian
-          film industry. <br /> <br /> The movie explores themes of love,
-          redemption, and sacrifice, capturing the essence of human emotions and
-          relationships. With a gripping narrative and memorable music,
-          &quot;Aawarapan&quot; has garnered a massive fan following both in
-          India and abroad, solidifying Emraan Hashmi&apos;s status as a
-          versatile actor.
         </p>
       );
     },
